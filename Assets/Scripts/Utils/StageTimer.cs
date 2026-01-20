1f160dbf6593a6732334b61a94b4d10793f9e11f
moonshine1004 <moonshine1004@naver.com>
2026-01-20 18:40:58 +0900

    fix Assets

--- Assets/Scripts/Utils/StageTimer.cs
+++ Assets/Scripts/Utils/StageTimer.cs
@@ -15,11 +15,32 @@ public class StageTimer : MonoBehaviour
     private float _remainingTime;
     private bool _isRunning = false;
 
+    #region Unity Lifecycle
+    private void Update()
+    {
+        if(_isRunning == false) return;
+
+        _remainingTime -= Time.deltaTime;
+
+        if(_remainingTime <= 0f)
+        {
+            _remainingTime = 0;
+            _isRunning = false;
+            EventBus.Instance.Publish<OnTimeOver>(new OnTimeOver());
+        }
+    }
+    #endregion
+
+
     public void StartTimer(float time)
     {
         _remainingTime = Mathf.Max(0, time);
         EventBus.Instance.Publish<OnTimeChange>(new OnTimeChange());
         _isRunning = true;
+    }
+    public void StopTimer()
+    {
+        _isRunning = false;
     }
 
 }
